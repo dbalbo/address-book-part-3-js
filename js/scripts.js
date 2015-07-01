851fc6ac06da94025f4e3590bc4e6ee5f1c275d0
debbie <debbalbo@hotmail.com>
2015-07-01 11:01:28 -0700

    specs pass for refactoring of inputted city state and contact name

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,21 @@
 function Contact(firstName, lastName) {
-  this.firstName = firstName
-  this.lastName = lastName
-  this.addresses =  []
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.addresses =  [];
 };
 
 Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
+};
+
+function Address(street, city, state) {
+  this.street = street;
+  this.city = city;
+  this.state = state;
+};
+
+Address.prototype.fullAddress = function() {
+  return this.street + ", " + this.city + ", " + this.state;
 };
 
 
@@ -52,10 +62,7 @@ $(document).ready(function() {
       newContact.addresses.push(newAddress);
     });
 
-    $("ul#contacts").append("<li><span class='contact'>" +
-                              newContact.firstName +
-                              " " + newContact.lastName +
-                              "</span></li>");
+    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
 
     $("input#new-first-name").val("");
     $("input#new-last-name").val("");
